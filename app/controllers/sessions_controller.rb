d3f5404a58443a45046e080212517eb2c307e5ef
K-Sonoda <kouki0911sonoda@gmail.com>
2024-08-13 02:45:43 +0000

    fix undefined method `session_token' for nil:NilClass

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,14 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      if user.activated?
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      if @user.activated?
         forwarding_url = session[:forwarding_url]
         reset_session
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        log_in user
-        redirect_to forwarding_url || user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        log_in @user
+        redirect_to forwarding_url || @user
       else
         message  = 'Account not activated. '
         message += 'Check your email for the activation link.'
